52fe74093c3f82ecfa0a74eaa274781c7acad528
Giotis Kal <giotisgr@gmail.com>
2016-05-15 14:02:07 +0300

    Finished CV sections

--- res/js/controllers.js
+++ res/js/controllers.js
@@ -1,4 +1,9 @@
 myApp.controller('resumeController', ['$scope', '$log', 'resumeService', function($scope, $log, resumeService) {
+    //Modal data
+    $scope.modalContent = {
+        heading: "",
+        body: ""
+    };
     //Fetch the resume data from the resume service
     $scope.resumeData = [];
     //if getResumeData is a promise then fetch the data
@@ -20,4 +25,54 @@ myApp.controller('resumeController', ['$scope', '$log', 'resumeService', functio
     else {
         $scope.resumeData = resumeService.getResumeData();
     }
+
+    /**
+    * Gets only the 4 first letters from the date string
+    * @return {string} year from date string
+    */
+    $scope.getYear = function(dateString) {
+        if (typeof dateString === 'undefined') return;
+        return dateString.substring(0, 4);
+    };
+
+    /**
+    * Opens the modal window setting its content
+    * @param {object} the content of the modal window
+    */
+    $scope.openModal = function(content) {
+        var modal = document.getElementById('modal'),
+            modalContainer = document.getElementById('modal__container'),
+            modalOverlay = document.getElementById('modal__overlay');
+
+        $scope.modalContent.heading = content.heading;
+        $scope.modalContent.body = content.body;
+
+        modal.classList.add('modal--is-open');
+        modalContainer.classList.add('scaleIn');
+        modalOverlay.classList.add('fadeIn');
+    };
+
+    /**
+    * Closes the modal window
+    */
+    $scope.closeModal = function() {
+        var modal = document.getElementById('modal'),
+            modalContainer = document.getElementById('modal__container'),
+            modalOverlay = document.getElementById('modal__overlay');
+
+        //modal.classList.remove('modal--is-open');
+        modalContainer.classList.remove('scaleIn');
+        modalOverlay.classList.remove('fadeIn');
+
+        modalContainer.classList.add('scaleOut');
+        modalOverlay.classList.add('fadeOut');
+
+        //Ugly timeout hack to reset animations, to get out of the trouble of intercepting
+        //an animationEnd event, while achieving a similarly seamless effect
+        setTimeout(function () {
+            modal.classList.remove('modal--is-open');
+            modalContainer.classList.remove('scaleOut');
+            modalOverlay.classList.remove('fadeOut');
+        }, 470);
+    };
 }]);
